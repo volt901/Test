43bbcfaea7511409e73c7716a50e04f5069d83c4
volt901 <89735853+volt901@users.noreply.github.com>
2023-05-05 23:39:40 +0500

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Владимир Ширяев on 05.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
